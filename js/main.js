df4a44aa07c111c3017e6e07baca5df5ebcc4c0e
wufan94 <278474781@qq.com>
2025-04-01 14:30:07 +0800

    1.2.9

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     particlesJS('particles-js', {
         particles: {
             number: {
-                value: 80,
+                value: 250,
                 density: {
                     enable: true,
                     value_area: 800
@@ -21,64 +21,59 @@ document.addEventListener('DOMContentLoaded', () => {
                 type: 'circle'
             },
             opacity: {
-                value: 0.5,
+                value: 0.3,
                 random: true,
-                animation: {
+                anim: {
                     enable: true,
-                    speed: 1,
+                    speed: 0.5,
                     opacity_min: 0.1,
                     sync: false
                 }
             },
             size: {
-                value: 3,
+                value: 2,
                 random: true,
-                animation: {
+                anim: {
                     enable: true,
-                    speed: 2,
-                    size_min: 0.1,
+                    speed: 1,
+                    size_min: 0.5,
                     sync: false
                 }
             },
             line_linked: {
-                enable: true,
-                distance: 150,
-                color: '#ffffff',
-                opacity: 0.4,
-                width: 1
+                enable: false
             },
             move: {
                 enable: true,
-                speed: 2,
+                speed: 1,
                 direction: 'none',
                 random: true,
                 straight: false,
                 out_mode: 'out',
-                bounce: false
+                bounce: false,
+                attract: {
+                    enable: true,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
             }
         },
         interactivity: {
-            detect_on: 'canvas',
+            detect_on: 'window',
             events: {
                 onhover: {
                     enable: true,
-                    mode: 'grab'
+                    mode: 'repulse'
                 },
                 onclick: {
-                    enable: true,
-                    mode: 'push'
+                    enable: false
                 },
                 resize: true
             },
             modes: {
-                grab: {
-                    distance: 140,
-                    line_linked: {
-                        opacity: 1
-                    }
-                },
-                push: {
-                    particles_nb: 4
+                repulse: {
+                    distance: 100,
+                    duration: 0.4
                 }
             }
         },
